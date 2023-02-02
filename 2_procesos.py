29deb6f9409c4514c1e457cbb6ae2ddcd6e08c0b
Judit <124157133+judnieto@users.noreply.github.com>
2023-02-02 10:28:54 +0100

    Add files via upload

---
+++ 2_procesos.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 10:07:42 2023
+
+@author: alumno
+"""
+import time
+import random
+
+from multiprocessing import Process
+from multiprocessing import current_process
+
+def f():
+    for i in range(5):
+        print (f"hola soy {current_process().name}, " + \
+               f"{current_process().pid}, {current_process().is_alive()}, vuelta {i}")
+        time.sleep(random.random()/3)
+        
+def g():
+    print ("adios")
+    
+if __name__ == "__main__":
+    N = 3
+    lp = []
+    for i in range(N):
+        lp.append(Process(target=f, name=f"ana_{i}"))
+    for p in lp:
+        p.start()
+    q = Process(target=g)
+    q.start()
+    print ("fin")
